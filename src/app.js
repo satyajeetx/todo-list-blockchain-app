997de4ef50d7cc5116aa953c98d54027da8aa023
Satyajeet Sindhiyani <sattu@isattu.com>
2022-05-02 22:00:32 +0530

    List Tasks

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 App = {
-  loading: false,
-  contracts: {},
-
+   loading: false,
+   contracts: {},
   load: async () => {
     await App.loadWeb3()
     await App.loadAccount()
     await App.loadContract()
     await App.render()
+    // await App.renderTasks()
   },
 
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   loadWeb3: async () => {
     if (typeof web3 !== 'undefined') {
+
       App.web3Provider = web3.currentProvider
       web3 = new Web3(web3.currentProvider)
     } else {
@@ -43,100 +44,73 @@ App = {
   },
 
   loadAccount: async () => {
-    // Set the current blockchain account
     App.account = web3.eth.accounts[0]
+//    console.log(App.account)
   },
 
-  loadContract: async () => {
-    // Create a JavaScript version of the smart contract
+  loadContract: async() => {
     const todoList = await $.getJSON('TodoList.json')
+//    console.log(todoList)
     App.contracts.TodoList = TruffleContract(todoList)
     App.contracts.TodoList.setProvider(App.web3Provider)
-
-    // Hydrate the smart contract with values from the blockchain
     App.todoList = await App.contracts.TodoList.deployed()
   },
 
   render: async () => {
-    // Prevent double render
-    if (App.loading) {
-      return
+    if(App.loading){
+        return
     }
-
-    // Update app loading state
     App.setLoading(true)
 
-    // Render Account
     $('#account').html(App.account)
 
-    // Render Tasks
     await App.renderTasks()
-
-    // Update loading state
     App.setLoading(false)
-  },
-
-  renderTasks: async () => {
-    // Load the total task count from the blockchain
-    const taskCount = await App.todoList.taskCount()
-    const $taskTemplate = $('.taskTemplate')
-
-    // Render out each task with a new task template
-    for (var i = 1; i <= taskCount; i++) {
-      // Fetch the task data from the blockchain
-      const task = await App.todoList.tasks(i)
-      const taskId = task[0].toNumber()
-      const taskContent = task[1]
-      const taskCompleted = task[2]
-
-      // Create the html for the task
-      const $newTaskTemplate = $taskTemplate.clone()
-      $newTaskTemplate.find('.content').html(taskContent)
-      $newTaskTemplate.find('input')
-                      .prop('name', taskId)
-                      .prop('checked', taskCompleted)
-                      .on('click', App.toggleCompleted)
-
-      // Put the task in the correct list
-      if (taskCompleted) {
-        $('#completedTaskList').append($newTaskTemplate)
-      } else {
-        $('#taskList').append($newTaskTemplate)
-      }
-
-      // Show the task
-      $newTaskTemplate.show()
-    }
-  },
-
-  createTask: async () => {
-    App.setLoading(true)
-    const content = $('#newTask').val()
-    await App.todoList.createTask(content)
-    window.location.reload()
-  },
-
-  toggleCompleted: async (e) => {
-    App.setLoading(true)
-    const taskId = e.target.name
-    await App.todoList.toggleCompleted(taskId)
-    window.location.reload()
   },
 
   setLoading: (boolean) => {
     App.loading = boolean
     const loader = $('#loader')
     const content = $('#content')
-    if (boolean) {
+    if(boolean){
       loader.show()
       content.hide()
-    } else {
+    }
+    else{
       loader.hide()
       content.show()
     }
+  },
+  renderTasks: async () =>{
+     // Load the total task from the blockchain
+     const taskCount = await App.todoList.taskCount()
+     const $taskTemplate = $('.taskTemplate')
+     // Render out each task with a new task template
+      for(var i = 1;i<=taskCount;i++){
+        const task = await App.todoList.tasks(i)
+        const taskId = task[0].toNumber()
+        const taskContent = task[1]
+        const taskCompleted = task[2]
+
+        const $newTaskTemplate = $taskTemplate.clone()
+        $newTaskTemplate.find('.content').html(taskContent)
+        $newTaskTemplate.find('input')
+                        .prop('name',taskId)
+                        .prop('checked',taskCompleted)
+                        // .prop('click',App.toggleCompleted)
+
+        if(taskCompleted){
+          $('#completedTaskList').append($newTaskTemplate)
+        }else{
+          $('#taskList').append($newTaskTemplate)
+        }
+        $newTaskTemplate.show()
+      }
+     // Show the task
+
+
   }
 }
-
 $(() => {
   $(window).load(() => {
     App.load()
